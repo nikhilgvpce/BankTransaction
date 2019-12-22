e554bb3c2bb70f596e20674a408f990b6dd0b2b6
Nikhil <nikhil.gvpce@gmail.com>
2019-12-23 01:01:09 +0530

    added pie chart

--- src/components/Analytics/Analytics.js
+++ src/components/Analytics/Analytics.js
@@ -1,10 +1,83 @@
-import React, {Component} from "react";
-import {DateBox} from "dev-extreme/react"
+import React, { Component } from "react";
+import { DateBox } from "devextreme-react";
+import PieChart, {
+    Legend,
+    Series,
+    Tooltip,
+    Format,
+    Label,
+    Connector,
+    Export
+  } from 'devextreme-react/pie-chart';
+import "./Analytics.css"
 
 export default class Analytics extends Component {
+
+    state = {
+        startDate: null,
+        endDate: null,
+        withDrawCount: 0,
+        depositCount: 0
+    }
+
+    gwtDate = (date) => {
+        const day = date[8] + date[9];
+        const month = date[4] + date[5] + date[6];
+        const year = date[11] + date[12] + date[13] + date[14];
+        return day + month + year;
+    }
+
+    setStartDate = (e) => {
+        this.setState(function (prevState, props) {
+            return { startDate: this.getDate(e.value) }
+        });
+    }
+
+    setEndDate = (e) => {
+        this.setState(function (prevState, props) {
+            return { endDate: this.getDate(e.value) }
+        });
+
+        if (this.state.startDate) {
+            this.getChartData();
+        }
+    }
+
+    getWithDrawCount = () => {
+        let count = 0;
+        this.props.data.forEach(transaction => {
+            if(transaction['Withdrawal AMT'] != null) {
+                count++;
+            }
+        });
+        return count;
+    }
+
+    getDepositCount = () => {
+        let count = 0;
+        this.props.data.forEach(transaction => {
+            if(transaction['Deposit AMT'] != null) {
+                count++;
+            }
+        });
+        return count;
+    }
+
+    getChartData = () => {
+        const withDrawCount = this.getWithDrawCount();
+        const depositCount = this.getDepositCount();
+        this.setState({withdisplayPie: true});
+    }
+
     render() {
         return (
-            
+            <div>
+                <p>Welcome to Analytics</p>
+                <div>
+                    <DateBox className="dateBox" placeholder="start date" onValueChanged={this.setStartDate} />
+                    <DateBox className="dateBox" placeholder="end date" onValueChanged={this.setEndDate} />
+                </div>
+            </div>
         )
     }
 }
--- src/components/TableData/TableData.js
+++ src/components/TableData/TableData.js
@@ -1,14 +1,23 @@
 import React, { Component } from "react";
 import DataGrid, { Paging, Pager } from 'devextreme-react/data-grid';
 import { Button, ButtonToolbar } from "react-bootstrap";
-
+import Analytics from "../Analytics/Analytics";
 
 export default class TableData extends Component {
+
+     state = {
+        showAnalytics: false
+     };
+
+    handleAnalytics = () => {
+        this.setState({showAnalytics: !this.state.showAnalytics});
+    }
+
     render() {
         return (
             <div>
                 <ButtonToolbar>
-                    <Button variant="primary">View Transaction Analytics</Button>
+                    <Button variant="primary" onClick={this.handleAnalytics}>View Transaction Analytics</Button>
                 </ButtonToolbar>
                 <DataGrid
                     dataSource={this.props.data}
@@ -20,6 +29,7 @@ export default class TableData extends Component {
                         allowedPageSizes={[5, 10, 20]}
                         showInfo={true} />
                 </DataGrid>
+                {this.state.showAnalytics ? <Analytics data={this.props.data}/> : null}
             </div>
         )
     }
